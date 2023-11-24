8d7bbbf129be9be7d304988b0a1381228b4dc4bc
mersad <mersad_998@yahoo.com>
2023-11-24 15:32:22 +0330

    fix: show characters binary

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+import { useState, type FC } from 'react';
+import { TextField, Typography } from '@mui/material';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+
+import { textToBinary } from './functions';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles(() => ({
+  gold: {
+    color: 'gold',
+  },
+  row: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  box: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    margin: '0 10px',
+    border: '1px solid gray',
+    paddingInline: 10,
+  },
+  textField: {
+    border: '1px solid white',
+  },
+}));
+
+const App: FC = () => {
+  const [plainText, setPlainText] = useState('Hello');
+
+  const classes = useStyles();
+
+  const onPlainTextChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setPlainText(event.target.value);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <Typography className={classes.gold} variant="h4" sx={{ marginBottom: '5rem' }}>
+        Data encryption standard (DES)
+      </Typography>
 
-export default App
+      <p>Enter plain text:</p>
+      <TextField
+        onChange={onPlainTextChange}
+        value={plainText}
+        sx={{ border: '1px solid gray' }}
+        inputProps={{ style: { color: 'white' } }}
+      />
+
+      <p>the plain text will break into separate characters</p>
+      <p>then 🤔</p>
+      <p>each character will turn to binary :</p>
+
+      <div className={classes.row}>
+        {plainText.split('').map((char) => (
+          <div className={classes.box}>
+            <p>{char === ' ' ? '-' : char}</p>
+            <ArrowDownwardIcon className={classes.gold} />
+            <p>{textToBinary(char)}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
